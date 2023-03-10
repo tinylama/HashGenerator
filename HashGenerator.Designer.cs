553c78f871c2cd18f05037f5f87d575f4b63ad39
Mark Coker <hello@tinycellar.com>
2023-03-11 00:39:20 +1100

    Add project files.

---
+++ HashGenerator.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace HashGenerator
+{
+    partial class HashGeneratorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HashGeneratorForm));
+            this.generateButton = new System.Windows.Forms.Button();
+            this.copyButton = new System.Windows.Forms.Button();
+            this.hashTextbox = new System.Windows.Forms.TextBox();
+            this.hashLabel = new System.Windows.Forms.Label();
+            this.saltTextbox = new System.Windows.Forms.TextBox();
+            this.saltLabel = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.hashTypePanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.hashTypeLabel = new System.Windows.Forms.Label();
+            this.hashTypesCombo = new System.Windows.Forms.ComboBox();
+            this.mainPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.passwordTextbox = new System.Windows.Forms.TextBox();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.generateSaltButton = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.hashTypePanel.SuspendLayout();
+            this.mainPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.generateSaltButton)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // generateButton
+            // 
+            this.generateButton.ForeColor = System.Drawing.Color.Black;
+            this.generateButton.Location = new System.Drawing.Point(84, 117);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(75, 23);
+            this.generateButton.TabIndex = 6;
+            this.generateButton.Text = "&Generate";
+            this.generateButton.UseVisualStyleBackColor = true;
+            this.generateButton.Click += new System.EventHandler(this.GenerateButton_Click);
+            // 
+            // copyButton
+            // 
+            this.copyButton.ForeColor = System.Drawing.Color.Black;
+            this.copyButton.Location = new System.Drawing.Point(3, 117);
+            this.copyButton.Name = "copyButton";
+            this.copyButton.Size = new System.Drawing.Size(75, 23);
+            this.copyButton.TabIndex = 5;
+            this.copyButton.Text = "&Copy";
+            this.copyButton.UseVisualStyleBackColor = true;
+            this.copyButton.Visible = false;
+            this.copyButton.Click += new System.EventHandler(this.CopyButton_Click);
+            // 
+            // hashTextbox
+            // 
+            this.hashTextbox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.hashTextbox.BackColor = System.Drawing.Color.Black;
+            this.hashTextbox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.hashTextbox.ForeColor = System.Drawing.Color.White;
+            this.hashTextbox.Location = new System.Drawing.Point(3, 91);
+            this.hashTextbox.Name = "hashTextbox";
+            this.hashTextbox.Size = new System.Drawing.Size(377, 20);
+            this.hashTextbox.TabIndex = 4;
+            this.hashTextbox.Visible = false;
+            // 
+            // hashLabel
+            // 
+            this.hashLabel.AutoSize = true;
+            this.hashLabel.Location = new System.Drawing.Point(3, 75);
+            this.hashLabel.Name = "hashLabel";
+            this.hashLabel.Size = new System.Drawing.Size(32, 13);
+            this.hashLabel.TabIndex = 3;
+            this.hashLabel.Text = "Hash";
+            this.hashLabel.Visible = false;
+            // 
+            // saltTextbox
+            // 
+            this.saltTextbox.BackColor = System.Drawing.Color.Black;
+            this.saltTextbox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.saltTextbox.Enabled = false;
+            this.saltTextbox.ForeColor = System.Drawing.Color.White;
+            this.saltTextbox.Location = new System.Drawing.Point(34, 52);
+            this.saltTextbox.Multiline = true;
+            this.saltTextbox.Name = "saltTextbox";
+            this.saltTextbox.Size = new System.Drawing.Size(97, 20);
+            this.saltTextbox.TabIndex = 2;
+            this.saltTextbox.TextChanged += new System.EventHandler(this.SaltChanged);
+            // 
+            // saltLabel
+            // 
+            this.saltLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.saltLabel.AutoSize = true;
+            this.saltLabel.Enabled = false;
+            this.saltLabel.Location = new System.Drawing.Point(3, 55);
+            this.saltLabel.Name = "saltLabel";
+            this.saltLabel.Size = new System.Drawing.Size(25, 13);
+            this.saltLabel.TabIndex = 1;
+            this.saltLabel.Text = "Salt";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.hashTypePanel);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(381, 43);
+            this.panel1.TabIndex = 0;
+            // 
+            // hashTypePanel
+            // 
+            this.hashTypePanel.Controls.Add(this.hashTypeLabel);
+            this.hashTypePanel.Controls.Add(this.hashTypesCombo);
+            this.hashTypePanel.Location = new System.Drawing.Point(0, 0);
+            this.hashTypePanel.Name = "hashTypePanel";
+            this.hashTypePanel.Size = new System.Drawing.Size(381, 42);
+            this.hashTypePanel.TabIndex = 0;
+            // 
+            // hashTypeLabel
+            // 
+            this.hashTypeLabel.AutoSize = true;
+            this.hashTypeLabel.Location = new System.Drawing.Point(3, 0);
+            this.hashTypeLabel.Name = "hashTypeLabel";
+            this.hashTypeLabel.Size = new System.Drawing.Size(59, 13);
+            this.hashTypeLabel.TabIndex = 0;
+            this.hashTypeLabel.Text = "Hash Type";
+            // 
+            // hashTypesCombo
+            // 
+            this.hashTypesCombo.BackColor = System.Drawing.Color.Black;
+            this.hashTypesCombo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.hashTypesCombo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.hashTypesCombo.ForeColor = System.Drawing.Color.White;
+            this.hashTypesCombo.FormattingEnabled = true;
+            this.hashTypesCombo.Location = new System.Drawing.Point(3, 16);
+            this.hashTypesCombo.Name = "hashTypesCombo";
+            this.hashTypesCombo.Size = new System.Drawing.Size(373, 21);
+            this.hashTypesCombo.TabIndex = 1;
+            this.hashTypesCombo.SelectedIndexChanged += new System.EventHandler(this.HashTypeChanged);
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.BackColor = System.Drawing.Color.Black;
+            this.mainPanel.Controls.Add(this.panel1);
+            this.mainPanel.Controls.Add(this.saltLabel);
+            this.mainPanel.Controls.Add(this.saltTextbox);
+            this.mainPanel.Controls.Add(this.generateSaltButton);
+            this.mainPanel.Controls.Add(this.passwordLabel);
+            this.mainPanel.Controls.Add(this.passwordTextbox);
+            this.mainPanel.Controls.Add(this.hashLabel);
+            this.mainPanel.Controls.Add(this.hashTextbox);
+            this.mainPanel.Controls.Add(this.copyButton);
+            this.mainPanel.Controls.Add(this.generateButton);
+            this.mainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainPanel.ForeColor = System.Drawing.Color.White;
+            this.mainPanel.Location = new System.Drawing.Point(0, 0);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Size = new System.Drawing.Size(384, 146);
+            this.mainPanel.TabIndex = 0;
+            // 
+            // passwordTextbox
+            // 
+            this.passwordTextbox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.passwordTextbox.BackColor = System.Drawing.Color.Black;
+            this.passwordTextbox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.passwordTextbox.ForeColor = System.Drawing.Color.White;
+            this.passwordTextbox.Location = new System.Drawing.Point(218, 52);
+            this.passwordTextbox.Multiline = true;
+            this.passwordTextbox.Name = "passwordTextbox";
+            this.passwordTextbox.Size = new System.Drawing.Size(161, 20);
+            this.passwordTextbox.TabIndex = 7;
+            this.passwordTextbox.TextChanged += new System.EventHandler(this.PasswordChanged);
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Location = new System.Drawing.Point(159, 55);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(53, 13);
+            this.passwordLabel.TabIndex = 8;
+            this.passwordLabel.Text = "Password";
+            // 
+            // generateSaltButton
+            // 
+            this.generateSaltButton.Image = ((System.Drawing.Image)(resources.GetObject("generateSaltButton.Image")));
+            this.generateSaltButton.Location = new System.Drawing.Point(137, 52);
+            this.generateSaltButton.Name = "generateSaltButton";
+            this.generateSaltButton.Size = new System.Drawing.Size(16, 16);
+            this.generateSaltButton.TabIndex = 9;
+            this.generateSaltButton.TabStop = false;
+            this.generateSaltButton.Click += new System.EventHandler(this.GenerateSalt);
+            // 
+            // HashGeneratorForm
+            // 
+            this.AcceptButton = this.generateButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 146);
+            this.Controls.Add(this.mainPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximumSize = new System.Drawing.Size(400, 185);
+            this.MinimumSize = new System.Drawing.Size(400, 185);
+            this.Name = "HashGeneratorForm";
+            this.ShowIcon = false;
+            this.Text = "Hash Generator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.hashTypePanel.ResumeLayout(false);
+            this.hashTypePanel.PerformLayout();
+            this.mainPanel.ResumeLayout(false);
+            this.mainPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.generateSaltButton)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button generateButton;
+        private System.Windows.Forms.Button copyButton;
+        private System.Windows.Forms.TextBox hashTextbox;
+        private System.Windows.Forms.Label hashLabel;
+        private System.Windows.Forms.TextBox saltTextbox;
+        private System.Windows.Forms.Label saltLabel;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.FlowLayoutPanel hashTypePanel;
+        private System.Windows.Forms.Label hashTypeLabel;
+        private System.Windows.Forms.ComboBox hashTypesCombo;
+        private System.Windows.Forms.FlowLayoutPanel mainPanel;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.TextBox passwordTextbox;
+        private System.Windows.Forms.PictureBox generateSaltButton;
+    }
+}
+
---
+++ HashGenerator.cs
@@ -0,0 +1,330 @@
+﻿using CryptSharp;
+using CryptSharp.Utility;
+using Konscious.Security.Cryptography;
+using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HashGenerator
+{
+    public partial class HashGeneratorForm : Form
+    {
+        public HashGeneratorForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadHashTypes();
+        }      
+
+        private void LoadHashTypes()
+        {
+            hashTypesCombo.Items.Add("MD5");
+            hashTypesCombo.Items.Add("SHA1");
+            hashTypesCombo.Items.Add("SHA256");
+            hashTypesCombo.Items.Add("SHA512");
+            hashTypesCombo.Items.Add("PHPASS");
+            hashTypesCombo.Items.Add("MYSQL5");
+            hashTypesCombo.Items.Add("NTLM");
+            hashTypesCombo.Items.Add("ARGON2");
+            hashTypesCombo.Items.Add("SCRYPT");
+            hashTypesCombo.Items.Add("BCRYPT");
+            hashTypesCombo.SelectedItem = "BCRYPT";
+        }
+
+        private static string GenerateSCRYPT(string password, string salt)
+        {
+            byte[] keyBytes = Encoding.UTF8.GetBytes(password);
+            byte[] saltBytes = Encoding.UTF8.GetBytes(salt);
+            int cost = 262144; //16384;
+            int blockSize = 8;
+            int parallel = 1; //Environment.ProcessorCount
+            var maxThreads = (int?)null;
+            var derivedKeyLength = 128;
+            return Convert.ToBase64String(SCrypt.ComputeDerivedKey(keyBytes, saltBytes, cost, blockSize, parallel, maxThreads, derivedKeyLength));
+            
+        }
+
+        private static string GeneratePHPASS(string password)
+        {
+            return PhpassCrypter.Phpass.Crypt(password);
+        }
+
+        private static string GenerateMD5(string password, string salt)
+        {
+            using (MD5 md5 = MD5.Create())
+            {
+                byte[] keyBytes = Encoding.ASCII.GetBytes(salt+password);
+                byte[] hashBytes = md5.ComputeHash(keyBytes);
+                return GetHex(hashBytes);
+            }
+        }
+
+        private static string GenerateSHA1(string password, string salt)
+        {
+            using (SHA1 sha1 = SHA1.Create())
+            {
+                byte[] inputBytes = Encoding.ASCII.GetBytes(salt + password);
+                byte[] hashBytes = sha1.ComputeHash(inputBytes);
+                return GetHex(hashBytes);
+            }
+        }
+
+        private static string GenerateSHA256(string password, string salt)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] inputBytes = Encoding.ASCII.GetBytes(salt + password);
+                byte[] hashBytes = sha256.ComputeHash(inputBytes);
+                return GetHex(hashBytes);
+            }
+        }
+        private static string GenerateSHA512(string password, string salt)
+        {
+            using (SHA512 sha512 = SHA512.Create())
+            {
+                byte[] inputBytes = Encoding.ASCII.GetBytes(salt+password);
+                byte[] hashBytes = sha512.ComputeHash(inputBytes);
+                return GetHex(hashBytes);
+            }
+        }
+
+        string GenerateMYSQL5(string password)
+        {
+            byte[] passBytes = Encoding.UTF8.GetBytes(password);
+            byte[] hash;
+            using (var sha1 = SHA1.Create())
+            {
+                hash = sha1.ComputeHash(passBytes);
+                hash = sha1.ComputeHash(hash);
+            }
+            return "*"+GetHex(hash);
+        }
+
+        private static string GetHex(byte[] hashBytes)
+        {
+            return BitConverter.ToString(hashBytes).Replace("-", "");
+        }
+               
+        private static string GetUniqueSalt(int size)
+        {
+            char[] chars =
+            "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".ToCharArray();
+            byte[] data = new byte[4 * size];
+            using (var crypto = RandomNumberGenerator.Create())
+            {
+                crypto.GetBytes(data);
+            }
+            StringBuilder result = new StringBuilder(size);
+            for (int i = 0; i < size; i++)
+            {
+                var rnd = BitConverter.ToUInt32(data, i * 4);
+                var idx = rnd % chars.Length;
+
+                result.Append(chars[idx]);
+            }
+
+            return result.ToString();
+        }
+
+        private static byte[] CreateSecureSalt()
+        {
+            var buffer = new byte[16];
+            var rng = new RNGCryptoServiceProvider();
+            rng.GetBytes(buffer);
+            return buffer;
+        }
+
+        private static string GenerateARGON2(string password, string salt)
+        {
+            string algorithm = "argon2id";
+            string version = "19";
+            var argon2 = new Argon2id(Encoding.UTF8.GetBytes(password))
+            {
+                Salt = Encoding.ASCII.GetBytes(salt),
+                DegreeOfParallelism = Environment.ProcessorCount,
+                Iterations = 4,
+                MemorySize = 1024 * 1024 // 1 GB                
+            };
+            
+            return $"${algorithm}$v={version}$m={argon2.MemorySize},t={argon2.Iterations},p={argon2.DegreeOfParallelism}${Convert.ToBase64String(argon2.Salt).Replace("=", "")}${Convert.ToBase64String(argon2.GetBytes(16)).Replace("=","")}";
+            //return GetHex(argon2.GetBytes(16));
+        }
+
+        private static string GenerateBCRYPT(string password)
+        {
+            return BCrypt.Net.BCrypt.EnhancedHashPassword(password);
+        }
+
+        private void RegenerateHash()
+        {
+            if (passwordTextbox.Text != "")
+            {
+                DisplayHash();
+                copyButton.Visible = true;
+                generateButton.Text = "&Clear";
+            }
+            
+        }
+
+        private void GenerateButton_Click(object sender, EventArgs e)
+        {
+            if (passwordTextbox.Text != "")
+            {
+                if (generateButton.Text == "&Generate")
+                {
+                    DisplayHash();
+                    copyButton.Visible = true;
+                    generateButton.Text = "&Clear";
+                }
+                else
+                {
+                    generateButton.Text = "&Generate";
+                    copyButton.Visible = false;
+                    hashTextbox.Clear();
+                    passwordTextbox.Clear();
+                    saltTextbox.Clear();
+                    hashLabel.Visible = false;
+                    hashTextbox.Visible = false;
+                }
+            }
+            else
+            {
+                hashLabel.Visible = true;
+                hashTextbox.Visible = true;
+                hashLabel.Text = "Information";
+                hashTextbox.Text = "Password is required.";
+            }
+        }
+
+            private void DisplayHash()
+        {
+            string salt = saltTextbox.Text;
+            string key = passwordTextbox.Text;
+            hashLabel.Visible = true;
+            hashTextbox.Visible = true;
+            hashLabel.Text = "Information";
+            hashTextbox.Text = "Calculating...";
+            string hash;
+            switch (hashTypesCombo.Text)
+            {
+
+                case "MD5":
+                    hash = GenerateMD5(key, salt); break;
+                case "SHA1":
+                    hash = GenerateSHA1(key, salt); break;
+                case "SHA256":
+                    hash = GenerateSHA256(key, salt); break;
+                case "SHA512":
+                    hash = GenerateSHA512(key, salt); break;
+                case "PHPASS":
+                    hash = GeneratePHPASS(key);
+                    break;
+                case "MYSQL5":
+                    hash = GenerateMYSQL5(key); break;
+                case "NTLM":
+                    hash = NTLM.GenerateNTLM(key); break;
+                case "BCRYPT":
+                    hash = GenerateBCRYPT(key); break;
+                case "ARGON2":
+                    if (salt == "")
+                    {
+                        salt = GetUniqueSalt(16);
+                        saltTextbox.Text = salt;
+                    }
+                    hashLabel.Text = "Information";
+                    hashTextbox.Text = "Calculating...";
+                    hash = GenerateARGON2(key, salt); 
+                    break;
+                case "SCRYPT":
+                    if (salt == "")
+                    {
+                        salt = GetUniqueSalt(16);
+                        saltTextbox.Text = salt;
+                    }
+                    hashLabel.Text = "Information";
+                    hashTextbox.Text = "Calculating...";
+                    hash = GenerateSCRYPT(key, salt);
+                    break;
+                default:
+                    hashLabel.Text = "Information";
+                    hash = "Hash type is required.";
+                    break;
+            }
+            hashLabel.Text = "Hash";
+            hashTextbox.Text = hash;
+            
+        }
+
+        private void CopyButton_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(hashTextbox.Text);
+        }
+
+        private void HasSalt()
+        {
+            bool salt;
+            switch (hashTypesCombo.Text)
+            {
+                case "MD5":
+                    salt = true; break;
+                case "SHA1":
+                    salt = true; break;
+                case "SHA256":
+                    salt = true; break;
+                case "SHA512":
+                    salt = true; break;
+                case "PHPASS":
+                    salt = false; break;
+                case "MYSQL5":
+                    salt = false; break;
+                case "NTLM":
+                    salt = false; break;
+                case "BCRYPT":
+                    salt = false; break;
+                case "ARGON2":
+                    salt = true; break;
+                case "SCRYPT":
+                    salt = true; break;
+                default: 
+                    salt = false; break;
+            }
+            saltLabel.Enabled = salt;
+            generateSaltButton.Visible = salt;
+            if (salt) { saltTextbox.BorderStyle = BorderStyle.FixedSingle; saltTextbox.Enabled = true; }
+            else{
+                saltTextbox.BorderStyle = BorderStyle.None;
+                saltTextbox.Text = "";
+                saltTextbox.Enabled = false;
+            }
+        }
+
+        private void HashTypeChanged(object sender, EventArgs e)
+        {
+            HasSalt();            
+            RegenerateHash();
+        }
+
+        private void SaltChanged(object sender, EventArgs e)
+        {
+            generateButton.Text = "&Generate";
+            copyButton.Visible = false;
+            hashTextbox.Clear();
+        }
+
+        private void GenerateSalt(object sender, EventArgs e)
+        {
+            saltTextbox.Text = GetUniqueSalt(16);
+        }
+
+        private void PasswordChanged(object sender, EventArgs e)
+        {
+            generateButton.Text = "&Generate";
+            copyButton.Visible = false;
+            hashTextbox.Clear();
+        }
+    }
+}
